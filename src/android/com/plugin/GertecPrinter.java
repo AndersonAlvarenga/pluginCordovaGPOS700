886dd7cdddfe2289b8fd9d750770c3e856d6ba19
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-09-01 12:33:01 -0300

    Desenvolvimento metodo onICL para iniciar ICl

--- src/android/com/plugin/GertecPrinter.java
+++ src/android/com/plugin/GertecPrinter.java
@@ -535,7 +535,7 @@ public class GertecPrinter {
         } catch (Exception e) {
             return "iCl.PowerOn - FAIL - " + e.getMessage();
         }
-        progressDialog = new ProgressDialog(MainActivity.this)
+        progressDialog = new ProgressDialog(MainActivity.this);
         progressDialog.setTitle("ICL");
         new Thread(new Runnable() {
             @Override
