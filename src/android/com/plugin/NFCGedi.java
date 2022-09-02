7590bff6a4f618b9d367fa26428c3a32d8d8e682
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-09-02 13:55:56 -0300

    Desenvolvimento metodo onICL para iniciar ICl

--- src/android/com/plugin/NFCGedi.java
+++ src/android/com/plugin/NFCGedi.java
@@ -74,8 +74,8 @@ public class NFCGedi extends Activity {
 
         IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected,tagDetected,ndefDetected, idDetected};
 
-        PendingIntent pendingIntent = PendingIntent.getActivity(
-                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        /*PendingIntent pendingIntent = PendingIntent.getActivity(
+                this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);*/
         if(nfcAdapter!= null)
             nfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
 
