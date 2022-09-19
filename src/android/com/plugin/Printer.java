17b6e5f12dc0f67287df82a1a9f0b0db25d4e8f1
AndersonAlvarenga <andersonalvarenga15@gmail.com>
2022-09-19 12:30:48 -0300

    Separando classe Print

--- src/android/com/plugin/Printer.java
+++ src/android/com/plugin/Printer.java
@@ -1,5 +1,6 @@
 package com.plugin;
 
+import android.app.Activity;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -33,6 +34,7 @@ public class Printer {
     private ConfigPrint configPrint;
     private Typeface typeface;
     private Context context;
+    private Activity activity;
 
     public Printer(Context context){
         this.context = context;
